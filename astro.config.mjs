9f56089f70ee65cdc4480a95b37d58d1d03eaaac
binaerverkehr <binaerverkehr@gmail.com>
2024-01-28 12:52:26 +0100

    Added an astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://testblogastro.netlify.app/",
+  site: "https://testblogastro.netlify.app/",
+  integrations: [preact()]
 });
